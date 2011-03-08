7cefa45ab92dc6da8ae41a702517fa8d229edcb6
Thomas Scholz <info@toscho.de>
2011-03-08 06:38:33 +0100

    Added out functions for help tabs.
    get_help() queries the data base,
    list_help() does the formatting and
    help_to_tab() replaces the help tab content with custom text if available.
    
    Signed-off-by: Thomas Scholz <info@toscho.de>

--- extended-help.php
+++ extended-help.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Extended Help
 Description: Help texts for super heroes
-Version:     0.1
+Version:     0.2
 Author:      Thomas Scholz
 Author URI:  http://toscho.de
 License:     GPL
@@ -24,6 +24,8 @@ class Extended_Help
 
 	/**
 	 * Handler for the action 'init'. Instantiates this class.
+	 *
+	 * @since 0.1
 	 * @return void
 	 */
 	public static function init()
@@ -41,15 +43,127 @@ class Extended_Help
 
 	/**
 	 * Basic setup.
+	 *
+	 * @since 0.1
 	 */
 	public function __construct()
 	{
 		$this->register_help_post_type();
 		$this->register_help_position();
+
+		add_filter( 'contextual_help', array ( $this, 'help_to_tab' ), 10, 3);
+	}
+
+	/**
+	 * Replaces the help tab content with custom text if available.
+	 *
+	 * @since 0.2
+	 * @param  string $contextual_help Default text
+	 * @param  string $screen_id
+	 * @param  object $screen
+	 * @return string
+	 */
+	public function help_to_tab( $contextual_help, $screen_id, $screen )
+	{
+		if ( ! ( $output = $this->list_help( $screen->id ) ) )
+		{
+			return $contextual_help;
+		}
+
+		return $output;
+	}
+
+	/**
+	 * HTML formatted help text. May be used for a shortcode.
+	 *
+	 * @since 0.2
+	 * @param  string $screen_id
+	 * @param  array $args 'heading_open', 'heading_close' and 'separator'
+	 * @return string|FALSE
+	 */
+	public function list_help( $screen_id = NULL, $args = array () )
+	{
+		if ( ! ( $help = $this->get_help( $screen_id ) ) )
+		{
+			return FALSE;
+		}
+
+		$defaults = array (
+			'heading_open'  => '<h3>'
+		,	'heading_close' => '</h3>'
+		,	'separator'     => '<hr />'
+		);
+
+		$options = array_merge( $defaults, $args );
+		$out     = array ();
+
+		while ( $help->have_posts() )
+		{
+			$text  = $help->next_post();
+			$out[] = $options['heading_open'] . $text->post_title . $options['heading_close']
+				. wpautop( do_shortcode( $text->post_content ) );
+		}
+		return implode( $options['separator'], $out );
+	}
+
+	/**
+	 * Queries the DB for help texts
+	 *
+	 * @since 0.2
+	 * @param  string $screen_id
+	 * @return object|FALSE
+	 */
+	public function get_help( $screen_id = NULL )
+	{
+		if ( is_null( $screen_id ) )
+		{
+			if ( isset ( $GLOBALS['menu']->id ) )
+			{
+				$screen_id = $GLOBALS['menu']->id;
+			}
+			else
+			{
+				return FALSE;
+			}
+		}
+
+		// @see http://codex.wordpress.org/Function_Reference/term_exists
+		$term = term_exists( $screen_id, $this->position_taxonomy );
+
+		// Screen not found.
+		// Should be a Term Array
+		// format: array( 'term_id' => term id, 'term_taxonomy_id' => taxonomy id )
+		if ( ! is_array( $term ) )
+		{
+			return FALSE;
+		}
+
+		$term_data = get_term( $term['term_id'], $this->position_taxonomy );
+
+		$args      = array (
+			'orderby'         => 'date'
+		,	'order'           => 'ASC'
+		,	'post_type'       => $this->post_type
+		// No limit.
+		,	'posts_per_page'  => -1
+		,	'taxonomy'        => $this->position_taxonomy
+		,	'term'            => $term_data->slug
+		);
+
+		$help = new WP_Query( $args );
+
+		if ( ! $help->have_posts() )
+		{
+			return FALSE;
+		}
+
+		return $help;
 	}
 
 	/**
 	 * Registers the custom post type for help texts.
+	 *
+	 * @since 0.1
 	 * @return void
 	 */
 	public function register_help_post_type()
@@ -79,6 +193,8 @@ class Extended_Help
 
 	/**
 	 * Registers the custom taxonomy for help texts.
+	 *
+	 * @since 0.1
 	 * @return void
 	 */
 	public function register_help_position()
